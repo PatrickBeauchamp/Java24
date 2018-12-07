2da80393b496ee7335be2e654df8e6af86e4f611
Patrick Beauchamp <patty.beauchamp@gmail.com>
2018-12-07 10:48:40 -0500

    modified id3

--- Java24/src/com/java24hours/ID3Reader.java
+++ Java24/src/com/java24hours/ID3Reader.java
@@ -3,24 +3,27 @@ package com.java24hours;
 import java.io.*;
 
 public class ID3Reader {
-
+    
     public static void main(String[] args) {
-        File song = new File("C:\\Users\\patty\\Downloads\\Yaka-anima_-_05_-_Mint_Tea_in_Sabilas_Garden.mp3");
+        File song = new File("C:/Users/patty/Downloads/Yaka-anima_-_05_-_Mint_Tea_in_Sabilas_Garden.mp3");
         try (FileInputStream file = new FileInputStream(song)) {
             int size = (int) song.length();
             file.skip(size - 128);
             byte[] last128 = new byte[128];
             file.read(last128);
             String id3 = new String(last128);
-            String tag = id3.substring(0,3);
+            String tag = id3.substring(0, 3);
             if (tag.equals("TAG")) {
-                System.out.println(id3.substring(3));
-            } else{
+                System.out.println("Title: " + id3.substring(3, 32));
+                System.out.println("Artist: " + id3.substring(33, 62));
+                System.out.println("Album: " + id3.substring(63, 91));
+                System.out.println("Year: " + id3.substring(93, 97));
+            } else {
                 System.out.println(" does not contain ID3 info...");
             }
             file.close();
-        }catch(IOException e){
-            System.out.println("Error" +e.toString());
+        } catch (IOException e) {
+            System.out.println("Error" + e.toString());
         }
     }
 }
